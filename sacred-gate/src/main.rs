d7f487a16498f032a2d5484cfcfabb64d33d6b23
XionWin <xionwin@hotmail.com>
2025-06-19 02:15:57 +0800

    Update

--- sacred-gate/src/main.rs
+++ sacred-gate/src/main.rs
@@ -1,4 +1,5 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     let r = ethereal_core::proto::GetUserByIdRequest {
         id: String::from("1")
     };
@@ -7,5 +8,10 @@ fn main() {
         id: String::from("1")
     };
     println!("Hello, {r:#?}!");
+
+    let user_service = arcane_vault::UserService::new().await;
+    let user = user_service.query_user_by_id("550e8400-e29b-41d4-a716-446655440000").await.expect("get user faild");
+    println!("Hello, {user:#?}!");
+
     println!("Hello, world!");
 }
