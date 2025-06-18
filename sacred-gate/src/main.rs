3989099904ae2c070b708e45af17e9737147a9d0
XionWin <xionwin@hotmail.com>
2025-06-19 02:27:01 +0800

    update

--- sacred-gate/src/main.rs
+++ sacred-gate/src/main.rs
@@ -1,5 +1,3 @@
-use uuid::Uuid;
-
 #[tokio::main]
 async fn main() {
     let r = ethereal_core::proto::GetUserByIdRequest {
@@ -11,7 +9,7 @@ async fn main() {
     };
     println!("Hello, {r:#?}!");
 
-    let uuid: Uuid = "550e8400-e29b-41d4-a716-446655440000".parse().ok().unwrap();
+    let uuid = "550e8400-e29b-41d4-a716-446655440000".parse::<uuid::Uuid>().ok().unwrap();
     let user_service = arcane_vault::UserService::new().await;
     let user = user_service
         .query_user_by_id(&uuid)
