461b18a04187170f34dde59453e4da41dc0099aa
XionWin <xionwin@hotmail.com>
2025-06-20 01:21:06 +0800

    Update

--- ethereal-core/build.rs
+++ ethereal-core/build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .file_descriptor_set_path("proto/my_service_descriptor.bin")
         .out_dir("src/proto")
         .compile_protos(&["user.proto", "user_profile.proto"], &["proto/"])
         .expect("Failed to compile protos");
--- sacred-gate/src/main.rs
+++ sacred-gate/src/main.rs
@@ -31,9 +31,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>  {
     )
     .parse()?;
 
+    let encoded_file_descriptor_set = include_bytes!("../../ethereal-core/proto/my_service_descriptor.bin");
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(encoded_file_descriptor_set)
+        .build_v1()?;
+
     let user_service = crate::domain::service::UserService::new().await;
 
     Server::builder()
+        .add_service(reflection_service)
         .add_service(ethereal_core::proto::user_service_server::UserServiceServer::new(user_service))
         .serve(addr)
         .await?;
