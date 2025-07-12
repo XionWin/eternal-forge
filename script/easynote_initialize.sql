20e7b3479968fd55737fd01e5499168f84e4f92f
XionWin <xionwin@hotmail.com>
2025-07-12 20:04:54 +0800

    Update

--- script/easynote_initialize.sql
+++ script/easynote_initialize.sql
@@ -1,10 +1,1 @@
-SELECT func_create_user(
-    'wangli@email.com',
-	'123456',
-    'Li',
-    'Wang',
-    1,
-    1,
-    'https://example.com/avatar1.png',
-    'Carpe diem.'
-);
+SELECT * FROM users inner join user_profiles on users.id = user_profiles.id;
